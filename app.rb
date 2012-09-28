4620465da032b5b7d07ef218537c6ad074e56189
Felipe JAPM <felipe.japm@gmail.com>
2012-09-28 03:21:42 -0300

    Deleted app.rb

--- app.rb
+++
@@ -1,9 +0,0 @@
-
-order_processing_service = AbacosIntegrationMonitor::OrderProcessingService.new
-integration_service = AbacosIntegrationMonitor::OrderIntegrationService.new
-order_processing_service.add_observer(integration_service)
-integration_service.add_observer(AbacosIntegrationMonitor::AlertMailer.new)
-loop {
-  order_processing_service.process
-  sleep(APP_CONFIG[:running_cycle_frequency])
-}
