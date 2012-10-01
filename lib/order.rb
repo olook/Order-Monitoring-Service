a659eb003989788a8c46c799d85340c5481ae8ea
Felipe JAPM <felipe.japm@gmail.com>
2012-10-01 11:50:21 -0300

    Added new scope (delayed) to order

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
 
- scope :from_checkpoint, lambda {|id| where('id > ?', id).limit(OrderMonitService::SERVICE_CONFIG[:max_orders_per_request]) }
-
+ scope :from_checkpoint, lambda {|id| where("id > ?", id).limit(OrderMonitService::SERVICE_CONFIG[:max_orders_per_request]) }
+ scope :delayed, lambda {|time| where("created_at <= '#{Time.now - time}'")}
 end
