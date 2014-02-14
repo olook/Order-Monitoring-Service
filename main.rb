5e13192525f70b481baf8f784de2105d1840d7b8
Rafael <rafael.manoel@gmail.com>
2014-02-14 12:40:38 -0200

    add log

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
-
-
+include OrderMonitService
 queueing_service = EnqueueingService.new(DATABASE_CONFIG, SERVICE_CONFIG[:max_attempts])
-integration_service = IntegrationService.new(INTEGRATION_SERVER_CONFIG,SERVICE_CONFIG[:downtime_wait])
+integration_service = IntegrationService.new(INTEGRATION_SERVER_CONFIG, SERVICE_CONFIG[:downtime_wait])
 queueing_service.add_observer(integration_service)
+puts EMAIL_CONFIG
 integration_service.add_observer(AlertMailer.new(EMAIL_CONFIG))
 loop {
   begin
