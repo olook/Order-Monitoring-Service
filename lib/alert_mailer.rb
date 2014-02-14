f0f8b1570986228785d34279d2b4768114f792bb
Rafael <rafael.manoel@gmail.com>
2014-02-14 12:45:31 -0200

    add log

--- lib/alert_mailer.rb
+++ lib/alert_mailer.rb
@@ -1,11 +1,13 @@
 class AlertMailer
 
     def initialize(email_config)
+      puts "AlertMailer inicializado: #{email_config}"
       @config = email_config
       Pony.options = {:via => :smtp, :via_options => @config[:smtp]}
     end
 
     def update(email)
+      puts "email para: #{email}"
       Pony.mail(:to => @config[:subscribers], :subject => email.subject,:html_body => email.html_body)
     end
 
